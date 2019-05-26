b4dac6d479c073db5cf4d43e4599034b925e68b0
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-05-26 14:29:24 -0500

    Spinkit, animacion de carga agregado

--- src/components/AppCartelera.vue
+++ src/components/AppCartelera.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    <section>
+  <main>
+    <aside v-if="!this.cartelera.length">
+      <div class="sk-cube-grid">
+        <div class="sk-cube sk-cube1"></div>
+        <div class="sk-cube sk-cube2"></div>
+        <div class="sk-cube sk-cube3"></div>
+        <div class="sk-cube sk-cube4"></div>
+        <div class="sk-cube sk-cube5"></div>
+        <div class="sk-cube sk-cube6"></div>
+        <div class="sk-cube sk-cube7"></div>
+        <div class="sk-cube sk-cube8"></div>
+        <div class="sk-cube sk-cube9"></div>
+      </div>
+    </aside>
+    <section v-else>
       <app-poster
         v-for="film in cartelera"
         :key="film.id"
         :poster="{ ...film, coleccion }"
       ></app-poster>
     </section>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -47,6 +60,17 @@ export default {
 </script>
 
 <style scoped>
+aside {
+  flex: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.sk-cube {
+  background-color: var(--Blanco);
+}
+
 section {
   margin: 1rem;
   display: grid;
