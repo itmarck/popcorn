f544c3026ff9d660db75a2a9c91bd2c43ade05f4
FabianXRL16 <fabianpacherres@gmail.com>
2019-05-28 08:33:36 -0500

    info detalle e imagenes de info

--- src/components/AppDetalle.vue
+++ src/components/AppDetalle.vue
@@ -1,35 +1,34 @@
 <template>
-  <div class="contenedor">
-    <div class="difuminado1">
-      <img class="fondo" :src="film.ruta" />
-    <div class="difuminado2">
-      <section class="imagen">
-        <img class="portada" :src="film.ruta" :alt="film.titulo" />
-      </section>
-      <div class="bg-modal" v-if="pop_up" >
-        <div class="modal-content">
-          <button class="pop_up__Cerrar" @click="pop_up=!pop_up" ><h1>X</h1></button>
-          <iframe width="853" height="480" :src="film.url" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="principal" :style="{ backgroundImage: `url(${film.ruta})` }">
+    <div class="sombra">
+      <div class="poster"><img :src="film.ruta"></div>
+      <div class="contenido">
+        <div class="contenido__header">
+          <div class="titulo">
+            <h1 class="titulo">{{ film.titulo }}</h1>
+            <button class="button__Cerrar" @click="cerrar"><h1>X</h1></button>
+          </div>
+          <div class="descripcion">
+            <ul>
+                <li v-for="(genero, i) of film.generos" :key="i">{{ genero }}</li>
+                <li>{{film.anio}}</li>
+                <li>{{ select.coleccion.toUpperCase() }}</li>
+            </ul>
+            <p class="sinopsis">{{ film.sinopsis }}</p>
+            <ul>
+              <li v-if="this.film.temporadas > 0">temp: {{ film.temporadas }}</li>
+              <li v-if="this.film.capitulos > 0">caps: {{ film.capitulos }}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="contenido__footer">
+          <button class="icofont-heart"></button>
+          <div class="switch"></div>
+          <button class="button__Trailer" id="button__Trailer" @click="pop_up=!pop_up" v-if="!pop_up"><h1>Ver trailer</h1></button>
         </div>
       </div>
-      <div class="contenedor__descripcion">
-        <button class="button__Cerrar" @click="cerrar"><h1>X</h1></button>
-        <h1 class="titulo">{{ film.titulo }}</h1>
-        <ul>
-            <li v-for="(genero, i) of film.generos" :key="i">{{ genero }}</li>
-            <li>{{film.anio}}</li>
-            <li>{{ select.coleccion.toUpperCase() }}</li>
-        </ul>
-        <p class="sinopsis__contenido">{{ film.sinopsis }}</p>
-        <ul>
-           <li v-if="this.film.temporadas > 0">temp: {{ film.temporadas }}</li>
-          <li v-if="this.film.capitulos > 0">caps: {{ film.capitulos }}</li>
-        </ul>
-        <button class="button__Trailer" id="button__Trailer" @click="pop_up=!pop_up" v-if="!pop_up"><h1>Ver trailer</h1></button>
-      </div>
     </div>
-    </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -70,140 +69,27 @@ export default {
 };
 </script>
 <style>
-.contenedor{
-  position: relative;
-  z-index: -1;
+.principal{
+  background-size: 100%;
 }
-.fondo{
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  filter: blur(12px);
-}
-.imagen {
-  position: relative;
-  margin-left: 80px;
-  margin-top: 49px;
-  margin-bottom: 40px;
-  width: 308px;
-  height: 462px;
-  border-radius: 6px;
-  z-index: 10;
-}
-.difuminado2{
-  display: flex;
+.sombra{
+  flex: auto;
   background: var(--Negro);
-  opacity: 0.8;
-  position: relative;
-  z-index: 0;
-}
-.portada{
-  height: 100%;
-}
-.button__Cerrar{
-  width: 30px;
-  height: 30px;
-  border: transparent;
-  background: transparent;
-  margin-top: 20px;
-  float: right;
-  margin-right: 20px;
-}
-.button__Cerrar h1{
-  font-size: 20px;
-  color: var(--colorClaro);
-  margin-top: auto;
-  margin-bottom: auto;
-  margin-left: auto;
-  margin-right: auto;
-}
-.button__Cerrar h1:hover{
-  color: var(--Blanco);
-}
-.contenedor__descripcion{
-  position: relative;
-  z-index: 100;
-}
-.titulo{
-  font-size: 40px;
-  text-transform: capitalize;
-  margin-top: 45.5px;
-  margin-left: 15px;
-  color: var(--Blanco); 
-  margin-bottom: 0;
-}
-ul{
-  margin-left: -40px;
-}
-ul li{
-  display: inline-block;
-  margin-left: 15px;
-  font-size: 10px;
-  text-transform: lowercase;
-  font-weight: initial;
-  margin-top: 0;
-}
-.sinopsis__contenido{
-  text-align: justify;
-  margin-left: 15px;
-  margin-top: 30px;
-  margin-right: 80px;
-  color:var(--Blanco); 
-}
-.button__Trailer{
-  width: 100px;
-	height: 35px;
-	padding: 0;
-	border-radius: 3px;
-	border: transparent 0.5px solid;
-  background: #1F375F;
-  display: inline-block;
-  position: absolute;
-  margin-left: 15px;
-  bottom: 46.5px;
-}
-.button__Trailer:hover{
-  background: var(--colorEnfasis);
-  z-index: 20%;
-}
-.button__Trailer h1{
-  font-size: 12px;
-  color: var(--Blanco);
-  margin-top: 8px;
-}
-.pop_up__Cerrar{
-  position: absolute;
-  top: 47px;
-  right: 235px;
-  width: 30px;
-  height: 30px;
-  border-radius:15px;
-  cursor:pointer;
-  background: transparent;
-}
-.pop_up__Cerrar h1{
-  font-size: 10px;
-  color: #11C4FF;
-  margin-top: auto;
-  margin-bottom: auto;
-  margin-left: auto;
-  margin-right: auto;
-}
-.bg-modal{
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0,0,0,0.7);
-  position: absolute;
-  top: 0;
+  opacity: 0.9;
+  max-height: inherit;  
   display: flex;
-  justify-content: center;
-  align-items: center;
+  padding-top: 4%;
+  padding-bottom: 3%;
+  padding-left: 10%;padding-right: 3%;
 }
-.modal-content{
-  width: 853px;
-  height: 480px;
-  background-color: white;
-  border-radius: 4px;
+.poster{
+  width: 30%;
+  height: 100%;
+}
+.poster img{
+  display: block;
+  width: auto;
+  height: 100%;
 }
 </style>
 
--- src/components/AppInfo.vue
+++ src/components/AppInfo.vue
@@ -1,6 +1,138 @@
 <template>
-  <div>
-    Info kbro marcelo
-    <img src="../../public/info/Vision.png" />
+<div class="contenedor">
+  <div class="principal">
+    <div class="cabecera">
+      <div class="banner">
+        <img class="portada" src="../../public/info/banner.png" />
+        <img class="titulo" src="../../public/info/titulo.png">
+      </div>
+    </div>
+    <div class="cuerpo">
+      <div class="cuerpo1">
+        <div class="mensaje">
+          <div class="mensaje__titulo">
+            <h1>Nuestro Objetivo</h1>
+          </div>
+          <div class="mensaje__mensaje">
+            <p>¡Transformamos una pelicula cualquiera en una historia extraordinaria!</p>
+          </div>
+        </div>
+        <div class="mensaje_img">
+          <img src="../../public/info/cuerpo_1.png" alt="">
+        </div>
+      </div>
+      <div class="cuerpo1">
+          <div class="mensaje2_img">
+            <img src="../../public/info/Vision.png" alt="">
+          </div>
+          <div class="mensaje2">
+            <div class="mensaje2__titulo">
+                <h1>Te Conectamos con la Pelicula</h1>
+          </div>
+        </div>
+      </div>
+      <div class="cuerpo1">
+        <div class="mensaje">
+          <div class="mensaje__titulo">
+            <h1>Míranos desde donde sea</h1>
+          </div>
+          <div class="mensaje__mensaje">
+            <p>Disponibles en Android y iPhone.</p>
+          </div>
+        </div>
+        <div class="mensaje_img">
+          <img src="../../public/info/cuerpo_2.png" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <h1>Integrantes</h1>
+      <ul>
+        <li>Marcelo Velásquez Villar</li>
+        <li>Javier Arturo Chávez Sialer</li>
+        <li>Fabían Andrés Pacherres Bautita </li>
+        <li>David Miguel Rioja Paz</li>
+        <li>Chritian Robert Torres Flores</li>
+      </ul>
+    </div>
   </div>
+</div>
 </template>
+<style> 
+.contenedor{
+  background: var(--Negro);
+  height: 2000px;
+}
+.principal{
+  width: 100%;
+  height: 1000px;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+.cabecera{
+  width: 100%;
+  position: relative;
+}
+.portada{
+  width: 100%;
+  display: block;
+}
+.titulo{
+width: 40%;
+position: absolute;
+bottom: 5%;
+left: 5%;
+}
+.cuerpo{
+  width: 100%;
+  height: 100%;
+}
+.cuerpo1{
+  max-height: inherit;  
+  display: flex;
+  margin-top: 10%;
+}
+.mensaje{
+  width: 30%;
+  height: 100%;
+  margin-top: 15%;
+}
+.mensaje__titulo h1{
+  font-size: 50px;
+  color: var(--colorEnfasis);
+  margin-bottom: 0;
+}
+.mensaje__mensaje{
+  font-size: 16px;
+}
+.mensaje_img{
+  width: 70%;
+  height: 55.6%;
+  display: flex;
+}
+.mensaje_img img{
+  width: 100%;
+  float: right;
+}
+.mensaje2_img{
+  width: 70%;
+  height: 55.6%;
+  display: flex;
+}
+.mensaje2_img img{
+  width: 80%;
+  float: right;
+}
+.mensaje2{
+  width: 40%;
+  height: 100%;
+  margin-top: 12%;
+}
+.mensaje2__titulo h1{
+  font-size: 50px;
+  color: var(--colorEnfasis);
+  margin-bottom: 0;
+  text-align: right;
+}
+
+</style>  
