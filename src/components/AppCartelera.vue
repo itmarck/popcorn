95446def99283006f7f4ac90ca82b61cf8a3dea3
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-05-27 21:53:17 -0500

    Refactor de app, cartelera, admin, poster, spinkit

--- src/components/AppCartelera.vue
+++ src/components/AppCartelera.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <aside v-if="!this.cartelera.length">
+    <div class="loader" v-if="!this.cartelera.length">
       <div class="sk-cube-grid">
         <div class="sk-cube sk-cube1"></div>
         <div class="sk-cube sk-cube2"></div>
@@ -12,14 +12,14 @@
         <div class="sk-cube sk-cube8"></div>
         <div class="sk-cube sk-cube9"></div>
       </div>
-    </aside>
-    <section v-else>
+    </div>
+    <div class="poster" v-else>
       <app-poster
         v-for="film in cartelera"
         :key="film.id"
         :poster="{ ...film, coleccion }"
       ></app-poster>
-    </section>
+    </div>
   </div>
 </template>
 
@@ -39,39 +39,40 @@ export default {
       cartelera: []
     };
   },
-  created() {
-    firebase
-      .firestore()
-      .collection(this.coleccion)
-      .orderBy("titulo")
-      .onSnapshot(snap => {
-        this.cartelera = [];
-        snap.forEach(doc => {
-          this.cartelera.push({
-            id: doc.id,
-            titulo: doc.data().titulo,
-            anio: doc.data().anio,
-            ruta: doc.data().ruta
+  methods: {
+    llenarCartelera() {
+      firebase
+        .firestore()
+        .collection(this.coleccion)
+        .orderBy("titulo")
+        .onSnapshot(snap => {
+          this.cartelera = [];
+          snap.forEach(doc => {
+            this.cartelera.push({
+              id: doc.id,
+              titulo: doc.data().titulo,
+              anio: doc.data().anio,
+              ruta: doc.data().ruta
+            });
           });
         });
-      });
+    }
+  },
+  created() {
+    this.llenarCartelera();
   }
 };
 </script>
 
 <style scoped>
-aside {
+.loader {
   flex: auto;
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
-.sk-cube {
-  background-color: var(--Blanco);
-}
-
-section {
+.poster {
   margin: 1rem;
   display: grid;
   grid-gap: 1rem;
@@ -79,37 +80,37 @@ section {
 }
 
 @media (min-width: 320px) and (max-width: 480px) {
-  section {
+  .poster {
     grid-template-columns: repeat(2, 1fr);
   }
 }
 
 @media (min-width: 480px) and (max-width: 640px) {
-  section {
+  .poster {
     grid-template-columns: repeat(3, 1fr);
   }
 }
 
 @media (min-width: 640px) and (max-width: 800px) {
-  section {
+  .poster {
     grid-template-columns: repeat(4, 1fr);
   }
 }
 
 @media (min-width: 800px) and (max-width: 1080px) {
-  section {
+  .poster {
     grid-template-columns: repeat(5, 1fr);
   }
 }
 
 @media (min-width: 1080px) and (max-width: 1270px) {
-  section {
+  .poster {
     grid-template-columns: repeat(6, 1fr);
   }
 }
 
 @media (min-width: 1270px) {
-  section {
+  .poster {
     grid-template-columns: repeat(7, 1fr);
   }
 }
