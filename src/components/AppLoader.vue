5b9b9049eb6175e9285cc3b5987116590d7e2216
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2019-06-02 04:22:52 -0500

    popcorn loader svg, spinkit quitado

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -1,16 +1,6 @@
 <template>
   <div class="loader">
-    <div class="sk-cube-grid">
-      <div class="sk-cube sk-cube1"></div>
-      <div class="sk-cube sk-cube2"></div>
-      <div class="sk-cube sk-cube3"></div>
-      <div class="sk-cube sk-cube4"></div>
-      <div class="sk-cube sk-cube5"></div>
-      <div class="sk-cube sk-cube6"></div>
-      <div class="sk-cube sk-cube7"></div>
-      <div class="sk-cube sk-cube8"></div>
-      <div class="sk-cube sk-cube9"></div>
-    </div>
+    <img class="pop" src="popcorn.svg" alt="" />
   </div>
 </template>
 
@@ -20,5 +10,9 @@
   display: flex;
   justify-content: center;
   align-items: center;
+}
+.pop {
+  display: block;
+  width: 250px;
 }
 </style>
